5212a2e6541c497794ccf6af4a95a27154dd6cae
Alexander Merkulov <sasha@merqlove.ru>
2016-07-28 14:00:31 +0300

    v0.1.5 Version bump :rocket: :rocket: :rocket:

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule RedixPubsubFastlane.Mixfile do
   use Mix.Project
 
   @project_url "https://github.com/merqlove/redix_pubsub_fastlane"
-  @version "0.1.4"
+  @version "0.1.5"
 
   def project do
     [app: :redix_pubsub_fastlane,
